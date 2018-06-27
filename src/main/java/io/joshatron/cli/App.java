9b11b1234341c7141af9536d0b24e28e57f8e9e5
Joshua Leger <legermjoshua@gmail.com>
2018-06-27 16:19:21 -0700

    added better help

--- src/main/java/io/joshatron/cli/App.java
+++ src/main/java/io/joshatron/cli/App.java
@@ -32,7 +32,22 @@ public class App
                 break;
             }
             else if(input.equals("help")) {
-                System.out.println("Type exit to quit the game.");
+                System.out.println("To place, the format is:");
+                System.out.println("\n    p[swc] [location]\n");
+                System.out.println("For [swc], s is stone, w is wall, and c is capstone.");
+                System.out.println("The location is in the format letter, then number.");
+                System.out.println("For example, the top left corner is A1. Case doesn't matter.");
+                System.out.println("An example place move would be 'ps C2'.");
+                System.out.println("This would place a regular stone at position C2.\n");
+                System.out.println("To move, the format is:");
+                System.out.println("\n    m[nsew] [location] g[pickup] [place first to last]\n");
+                System.out.println("For [nsew], n is north(up), s is south(down), e is east(right), and w is west(left)");
+                System.out.println("The location is the spot to pick up from. It is the same format as in place.");
+                System.out.println("Pickup is the number of pieces to pick up from the start location");
+                System.out.println("Place first to last is how many to place at each spot, separate by spaces.");
+                System.out.println("An example move would be 'mn A3 g3 2 1'.");
+                System.out.println("This would pick up 3 pieces from A3, place 2 on A2, and one on A1.\n");
+                System.out.println("To quit the game, type 'exit'");
             }
             else {
                 Turn turn = turnFromString(input);
@@ -90,12 +105,12 @@ public class App
             int x = xToNum(str.charAt(3));
             int y = charToNum(str.charAt(4)) - 1;
 
-            int pickUp = charToNum(str.charAt(6));
+            int pickUp = charToNum(str.charAt(7));
 
             int spots = (str.length() - 7) / 2;
             int[] drop = new int[spots];
             for(int i = 0; i < spots; i++) {
-                drop[i] = charToNum(str.charAt(7 + (2 * i) + 1));
+                drop[i] = charToNum(str.charAt(8 + (2 * i) + 1));
             }
 
             if(dir != null && x != -1 && y != -1 && pickUp != -1) {
